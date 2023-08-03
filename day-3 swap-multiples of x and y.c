94c8ceb0ef06057b8b9fa7136abe0a5c7f10a651
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2023-08-03 12:20:13 +0530

    Add files via upload

---
+++ day-3 swap-multiples of x and y.c
@@ -0,0 +1,84 @@
+/*
+The program must accept N integers and two integers X and Y as the input. The program must swap the first occurring mutiple of X and the last occurring multiple of Y among the N integers. Then the program must print modified values of the N integers as the output Note
+
+At least one multiple of X and Y are always present in the N integers.
+
+Boundary Conditions)
+
+	2 <= N <= 100
+	
+	1 <= X Y <= 100
+
+	1 c Each integer value <= 10^8
+
+Input Format
+
+	The first line contains the value of N.
+
+	The second line contains the value of N integers separated by space(s). The third line contains the value of X and Y separated by space(s).
+
+Output Format:
+
+	The first line contains the modified value of N integers as per the conditions.
+
+Example Input/Output 1:
+
+	Input:
+
+		10
+
+		13 28 76 34 86 77 18 92 57 10
+
+		7 11
+
+	Output
+
+		13 77 76 34 86 28 18 92 57 10
+
+	Explanation:
+
+		The first occurring multiple of 7 among the 10 integers is 20. The last occurring multiple of 11 among the 10 integers is 77. So these two values are swaped in their positions. Hence the output is 
+13 77 76 34 86 28 18 92 57 10
+*/
+
+
+//answer
+
+#include<stdio.h> 
+#include<stdlib.h>
+
+int main()
+
+{
+	int n;
+	scanf("%d",&n);
+	int a[n]; 
+	for (int i=0;i<n;i++) 
+		scanf("%d",&a[i]);
+	int x,y; 
+	scanf("%d %d",&x,&y); 
+	int r,f; 
+	for (int i=0;i<n;i++)
+	{
+		if(a[1]xx--)
+		{
+			r=i; 
+			break;
+		}
+	}
+	for (int 1-n-1;10;1--)
+	{
+		if(a[i]xy==0)
+		{
+			f=i; 
+			break;
+		}
+	}
+
+	int t=a[r]; 
+	a[r]=a[f];
+	a[f]=t; 
+	for (int i=0;i<n;i++) 
+		printf("%d,a[i]);
+}
+	
