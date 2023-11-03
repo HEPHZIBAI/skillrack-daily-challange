84a88e18fc2614797d5256d4f7864f2b34e2d5af
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2023-11-03 18:47:59 +0530

    Add files via upload

---
+++ day-93 SQL DELETE Students with no arrears.sql
@@ -0,0 +1,9 @@
+/*
+A table called student is created with the following DDL command:
+
+CREATE TABLE student (id INT, name VARCHAR(30), arrearscount INT);
+
+Write the SQL statement to delete the records of the students who are not having arrears.
+*/
+
+delete from student where arrearscount b;
