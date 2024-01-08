7fbcb8c7a798e7d83be9ccf03d4c1abcda70d8e0
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2024-01-08 15:59:20 +0530

    Add files via upload

---
+++ day-159 Python - C - 012.c
@@ -0,0 +1,10 @@
+/*
+
+Please convert the following Python code to C so that the C program executes successfully passing the test cases.
+
+N = int(input())
+numList = [int(val) for val in input().split()]
+newList = list(map(lambda X: X*2 if X % 2 == 0 else X**2,newList))
+print(*newList)
+
+*/
